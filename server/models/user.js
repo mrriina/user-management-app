7db07e5fad06c709e484561e745c6f407bcfbf3a
mrriina <mariaip12345678911@gmail.com>
2023-10-07 23:14:44 +0300

    creating a project structure, configuring routes

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+const sequelize = require('../db')
+const {DataTypes, DATE} = require('sequelize')
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
+    name: {type: DataTypes.STRING, require:true},
+    email: {type: DataTypes.STRING, require:true, unique:true},
+    password: {type: DataTypes.STRING, require:true},
+    signUp: {type: DataTypes.DATE, defaultValue: DATE.now},
+    signIn: {type: DataTypes.DATE, defaultValue: DATE.now},
+    status: {type: DataTypes.STRING, defaultValue:'unblock'},
+})
+
+module.exports = {User}
