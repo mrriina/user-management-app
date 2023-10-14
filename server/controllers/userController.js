f63631a94cd93e96ac23c36aed515de1c9c60ae9
mrriina <mariaip12345678911@gmail.com>
2023-10-14 15:59:19 +0300

    login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -37,6 +37,7 @@ class UserController {
 
             const hashPassword = await bcrypt.hash(password, 5)
             const user = await User.create({name, email, password: hashPassword})
+            console.log('process.env.SECRET_KEY==', process.env.SECRET_KEY);
             const token = generateJwt(user.id, email)
             
             return res.json({token,
